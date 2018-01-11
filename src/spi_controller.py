edd620fc722447a5cf45735a0c1b86f3339a40bc
Niklas Fessler <niklas@appwerkstatt.net>
2018-01-11 16:15:28 +0100

    changed index

--- src/spi_controller.py
+++ src/spi_controller.py
@@ -28,15 +28,15 @@ def send_receive_over_spi(player, message):
     try:
         print("send:", message)
         resp = spi.xfer([(message[0].value & 0xFF),message[1] & 0XFF, 0x00, 0x00])
-        if resp:
+        if resp == 4:
             tmp = 0
-            index = 0
+            index = 3
             for item in resp:
                 tmp = merge_hex(tmp, reverse_bits(item), index)
-                index += 1
+                index -= 1
             result = str(tmp).encode()
             print(result)
-            player.connection.send(result)   
+            #player.connection.send(result)   
         #end while
     except KeyboardInterrupt:
         spi.close()
